d46eb1901a637109e42d77580f76656e3524d0fd
lchannng <l.channng@gmail.com>
2022-12-03 21:29:23 +0800

    automatically commit and push

--- run.sh
+++ run.sh
@@ -8,3 +8,6 @@ m3u_file="https://iptv-org.github.io/iptv/countries/cn.m3u"
 # m3u_file="https://raw.githubusercontent.com/frankwuzp/iptv-cn/main/tv-ipv4-cmcc.m3u"
 mkdir -p ./output
 docker run --rm -v `pwd`/output:/data -e PUID=1000 -e PGID=1000 lchannng/iptv-checker -d -p 32 -o /data/ ${m3u_file}
+
+git add output/ && git commit -m "`date +"%Y-%m-%dT%H:%M:%S%z"`"
+git push
